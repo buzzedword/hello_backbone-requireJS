495b91d09966b2b0c454383febae621c8e2f01e7
Daniel Garcia <danny@noisenewyork.com>
2011-11-02 15:50:56 -0400

    Modified for better dependancy

--- js/main/main.js
+++ js/main/main.js
@@ -1,22 +1,27 @@
-require({
-	paths : {
-		'jquery' : location.protocol + '//cdnjs.cloudflare.com/ajax/libs/jquery/1.6.4/jquery.min',
-		'underscore' : location.protocol + '//cdnjs.cloudflare.com/ajax/libs/underscore.js/1.1.7/underscore-min',
-		'backbone' : location.protocol + '//cdnjs.cloudflare.com/ajax/libs/backbone.js/0.5.3/backbone-min'
+require(
+	{
+		baseUrl: 'js/main',
+		paths : {
+			'jquery' : location.protocol + '//cdnjs.cloudflare.com/ajax/libs/jquery/1.6.4/jquery.min',
+			'underscore' : location.protocol + '//cdnjs.cloudflare.com/ajax/libs/underscore.js/1.1.7/underscore-min',
+			'backbone' : location.protocol + '//cdnjs.cloudflare.com/ajax/libs/backbone.js/0.5.3/backbone-min'
+		}
+	},
+	["main"],
+	function(){
+		require(['jquery', 'underscore', 'backbone'], function(){
+			Backbone.sync = function(method, model, success, error){
+				success();
+			}
+			
+			require([
+				'Collection/list',
+				'Model/item',
+				'View/itemView',
+				'View/listView'
+			], function(List, Item, ItemView, ListView){
+				var listView = new ListView();
+			});
+		});
 	}
-});
-
-require(['jquery', 'underscore', 'backbone'], function(){
-	Backbone.sync = function(method, model, success, error){
-		success();
-	}
-	
-	require([
-		'Collection/list',
-		'Model/item',
-		'View/itemView',
-		'View/listView'
-	], function(List, Item, ItemView, ListView){
-		var listView = new ListView();
-	});
-});
+);
